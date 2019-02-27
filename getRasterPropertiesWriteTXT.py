787f8fd03492fb4721af8d7a06d51b090c68e0fa
sanozmen <s.sanozmen@hotmail.com>
2019-02-27 11:33:47 +0300

    finalCodeCommit

---
+++ getRasterPropertiesWriteTXT.py
@@ -0,0 +1,14 @@
+import arcpy
+outputTxtFile =  r'C:\Users\xx.txt' #output comma delimited text file
+arcpy.env.workspace = r'set your env'#input raster file
+rasters = arcpy.ListRasters("*")
+print rasters
+for raster in rasters:
+    minResult = arcpy.GetRasterProperties_management(raster, "MINIMUM")
+    maxResult = arcpy.GetRasterProperties_management(raster, "MAXIMUM")
+    meanResult = arcpy.GetRasterProperties_management(raster, "MEAN")
+    minRes = minResult.getOutput(0)
+    maxRes = maxResult.getOutput(0)
+    meanRes = meanResult.getOutput(0)
+    f = open(outputTxtFile, 'a+') #open the text file for writting
+    f.write(raster+","+minRes+","+meanRes+","+maxRes+"\n")
